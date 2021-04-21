21403d64f3245b13fbe98dc41b8561d5bd778159
Tscunami <lukasnaplava96@gmail.com>
2021-04-21 15:56:41 +0200

    Edit Google Form selectors, Add Doctstring

--- bezrealitky.py
+++ bezrealitky.py
@@ -7,8 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
 from my_selectors import *
 
-STANDARD_WAIT_TIME = 3
-CLICK_SLEEP_TIME = 0.25
+WAIT_TIME_FOR_ELEMENT_TO_LOAD = 3
 CITY = 'Praha'
 PRICE_FROM = 10000
 PRICE_TO = 20000
@@ -23,53 +22,85 @@ class BezRealitky():
         self.driver = webdriver.Chrome(executable_path="C:\Shortcuts\Applications/chromedriver.exe",
                                        chrome_options=self.option)
 
-    def perform_action(self, name, action, selector, wait_time=STANDARD_WAIT_TIME):
+    def perform_action(self, selector_name, action, selector_type, wait_time=WAIT_TIME_FOR_ELEMENT_TO_LOAD):
+        """
+        waits and locates element, then performs desired action
+        :param selector_name: it can be full xpath or name of id
+        :param action: click or values for action send_keys
+        :param selector_type: id or xpath
+        :param wait_time: by default is 3s
+        """
         if action == "click":
-            if selector == "id":
+            if selector_type == "id":
                 try:
                     WebDriverWait(self.driver, wait_time).until(
-                        EC.element_to_be_clickable((By.ID, name))).click()
+                        EC.element_to_be_clickable((By.ID, selector_name))).click()
                 except TimeoutException:
-                    print(f"{name} element not loaded")
+                    print(f"{selector_name} element not loaded")
                 except ElementClickInterceptedException:
-                    print(f"{name} element raised ElementClickInterceptedException")
-
-            elif selector == "xpath":
+                    print(f"{selector_name} element raised ElementClickInterceptedException")
+            elif selector_type == "xpath":
                 try:
                     WebDriverWait(self.driver, wait_time).until(
-                        EC.element_to_be_clickable((By.XPATH, name))).click()
+                        EC.element_to_be_clickable((By.XPATH, selector_name))).click()
                 except TimeoutException:
-                    print(f"{name} element not loaded")
+                    print(f"{selector_name} element not loaded")
                 except ElementClickInterceptedException:
-                    print(f"{name} element raised ElementClickInterceptedException")
+                    print(f"{selector_name} element raised ElementClickInterceptedException")
+            elif selector_type == "css":
+                try:
+                    WebDriverWait(self.driver, wait_time).until(
+                        EC.element_to_be_clickable((By.CSS_SELECTOR, selector_name))).click()
+                except TimeoutException:
+                    print(f"{selector_name} element not loaded")
+                except ElementClickInterceptedException:
+                    print(f"{selector_name} element raised ElementClickInterceptedException")
         else:
-            try:
-                WebDriverWait(self.driver, wait_time).until(
-                    EC.presence_of_element_located((By.ID, name))).send_keys(action)
-            except TimeoutException:
-                print(f"{name} element not loaded")
-            except ElementClickInterceptedException:
-                print(f"{name} element raised ElementClickInterceptedException")
+            if selector_type == "id":
+                try:
+                    WebDriverWait(self.driver, wait_time).until(
+                        EC.presence_of_element_located((By.ID, selector_name))).send_keys(action)
+                except TimeoutException:
+                    print(f"{selector_name} element not loaded")
+                except ElementClickInterceptedException:
+                    print(f"{selector_name} element raised ElementClickInterceptedException")
+            elif selector_type == "xpath":
+                print("searching with xpath")
+                try:
+                    WebDriverWait(self.driver, wait_time).until(
+                        EC.presence_of_element_located((By.XPATH, selector_name))).send_keys(action)
+                except TimeoutException:
+                    print(f"{selector_name} element not loaded")
+                except ElementClickInterceptedException:
+                    print(f"{selector_name} element raised ElementClickInterceptedException")
 
     def start_searching(self):
+        """
+        manages whole process
+        """
         self.driver.get("https://www.bezrealitky.cz/")
-        sleep(0.75)
         self.apply_filters()
         self.read_all_offers()
         self.download_html()
 
     def close_advertisement(self):
-        advertisement = WebDriverWait(self.driver, 20).until(
+        """
+        closes advertisement banner
+        """
+        advertisement = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable(
                 (By.XPATH, "/html/body/div[11]/div/div/div[2]/div/a/img")))
         self.driver.execute_script("arguments[0].click();", advertisement)
 
     def apply_filters(self):
-        self.driver.find_element_by_id(TYPE_PRONAJEM_ID).click()
-        self.driver.find_element_by_id(TYPE_BYT_ID).click()
-        self.driver.find_element_by_id(LOCATION_INPUT_ID).send_keys(CITY)
+        """
+        applies all desired filters
+        """
+        self.perform_action(TYPE_PRONAJEM_ID, "click", "id")
+        self.perform_action(TYPE_BYT_ID, "click", "id")
+        self.perform_action(LOCATION_INPUT_ID, "click", "id")
+        self.perform_action(TYPE_PRONAJEM_ID, "click", "id")
         self.driver.find_element_by_id(LOCATION_INPUT_ID).submit()
-
         self.perform_action(PRICE_FROM_ID, PRICE_FROM, "id")
         self.perform_action(PRICE_TO_ID, PRICE_TO, "id")
         self.perform_action(TYPE_OF_HOUSING_XPATH, "click", "xpath")
@@ -88,23 +119,24 @@ class BezRealitky():
         self.perform_action(ACCEPT_COOKIES_ID, "click", "id", 4)
 
     def read_all_offers(self):
+        """
+        scrolls through all offers
+        """
         are_next_offers = True
         while are_next_offers:
             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
             sleep(1)
-
             try:
-                self.driver.find_element_by_css_selector("#search-content > form > div.b-filter__content "
-                                                 "> div.b-filter__inner.pb-0 > div.row > div "
-                                                 "> p > button").click()
+                self.driver.find_element_by_css_selector(NEXT_OFFERS_BUTTON_XPATH).click()
             except NoSuchElementException:
                 are_next_offers = False
             sleep(1)
 
     def download_html(self):
-        html_content = self.driver.find_element_by_xpath("/html/body/div[1]/main/div/div/div[2]/form/div"
-                                                         "[2]/div[4]").get_attribute('outerHTML')
-
+        """
+        downloads html with all offers and put each address, price and url link in lists
+        """
+        html_content = self.driver.find_element_by_xpath(HTML_CONTENT_XPATH).get_attribute('outerHTML')
         soup = BeautifulSoup(html_content, "html.parser")
         articles = soup.find_all(name="a", class_="product__link")
         prices = soup.find_all(name="strong", class_="product__value")
@@ -116,21 +148,20 @@ class BezRealitky():
         self.send_data(addresses=all_addresses, prices=all_prices, links=all_links)
 
     def send_data(self, addresses, prices, links):
-
+        """
+        submits all apartments in google forms
+        :param addresses: list of all addresses
+        :param prices: list of all prices
+        :param links: list of all url links
+        """
         for index in range(0, len(addresses)):
             self.driver.get(
                 "https://docs.google.com/forms/d/e/1FAIpQLSdYR-xloQsQdsHOSE9Kld4MO4OwbbqA7ehE2eDgevfQhe0e"
                             "6w/viewform?usp=sf_link"
             )
-            sleep(1)
-            self.driver.find_element_by_xpath("/html/body/div/div[2]/form/div[2]/div/div[2]/div[1]/div/div/div[2]"
-                                              "/div/div[1]/div/div[1]/input").send_keys(addresses[index])
-            self.driver.find_element_by_xpath("/html/body/div/div[2]/form/div[2]/div/div[2]/div[2]/div/div/div[2]"
-                                              "/div/div[1]/div/div[1]/input").send_keys(prices[index])
-            self.driver.find_element_by_xpath("/html/body/div/div[2]/form/div[2]/div/div[2]/div[3]/div/div/div[2]"
-                                              "/div/div[1]/div/div[1]/input").send_keys(links[index])
-            sleep(0.25)
-            self.driver.find_element_by_xpath("/html/body/div/div[2]/form/div[2]/div/div[3]/div[1]/div/div/span"
-                                              "/span").click()
+            self.perform_action(ADDRESS_XPATH, addresses[index], "xpath")
+            self.perform_action(PRICES_XPATH, prices[index], "xpath")
+            self.perform_action(URL_XPATH, links[index], "xpath")
+            self.perform_action(SUBMIT_XPATH, "click", "xpath")
 
 
--- my_selectors.py
+++ my_selectors.py
@@ -1,3 +1,4 @@
+# Bezrealitky elements
 TYPE_PRONAJEM_ID = "a-type-pronajem"
 TYPE_BYT_ID = "a-type-byt"
 LOCATION_INPUT_ID = "locationInput"
@@ -11,4 +12,14 @@ SURFACE_FROM_ID = "surfaceFrom"
 SEARCH_BUTTON_XPATH = "//*[@id='search-content']/form/div[2]/div[2]/div[3]/div[2]/p/button"
 ACCEPT_COOKIES_ID = "onetrust-accept-btn-handler"
 CLOSE_ADVERTISEMENT_XPATH = "/html/body/div[11]/div/div/div[2]/div/a/img"
+HTML_CONTENT_XPATH = "/html/body/div[1]/main/div/div/div[2]/form/div[2]/div[4]"
+NEXT_OFFERS_BUTTON_XPATH = "#search-content > form > div.b-filter__content > div.b-filter__inner.pb-0 " \
+                           "> div.row > div > p > button"
+
+# Google Form elements
+ADDRESS_XPATH = "/html/body/div/div[2]/form/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input"
+PRICES_XPATH = "/html/body/div/div[2]/form/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input"
+URL_XPATH = "/html/body/div/div[2]/form/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input"
+SUBMIT_XPATH = "/html/body/div/div[2]/form/div[2]/div/div[3]/div[1]/div/div/span/span"
+
 
